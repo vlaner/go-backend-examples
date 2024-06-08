62737a304036e6a01dfd060cc9aeb4390e7d235c
vlaner <43291931+vlaner@users.noreply.github.com>
2024-06-08 13:13:13 +0300

    add test to ci cd

--- github_ci_cd/transform_test.go
+++ github_ci_cd/transform_test.go
@@ -12,6 +12,16 @@ func TestTransformFunction(t *testing.T) {
 	}
 }
 
+func TestOnlyNumersInString(t *testing.T) {
+	inputStr := "1234"
+	expected := "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+
+	transformed := TransformString(inputStr)
+	if expected != transformed {
+		t.Errorf("%s != %s", expected, transformed)
+	}
+}
+
 func TestEmptyString(t *testing.T) {
 	inputStr := ""
 	expected := "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
